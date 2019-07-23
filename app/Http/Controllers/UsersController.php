0bc6c129d475f0344e0a1ba3ed5d4b1a3324e64e
phpervip <2998658517@qq.com>
2019-07-23 06:48:12 +0000

    中文的错误提示信息

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -21,11 +21,15 @@ class UsersController extends Controller
     public function store(Request $request)
     {
         $this->validate($request,[
-                'name'=>'require|max:50',
-                'email'=>'require|email|unique:users|max:255',
-                'password'=>'require|confirmed|min:6',
+                'name'=>'required|max:50',
+                'email'=>'required|email|unique:users|max:255',
+                'password'=>'required|confirmed|min:6'
 
         ]);
         return;
     }
+
+
+
+
 }
--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -6,8 +6,8 @@
       <div class="card-header">
         <h5>注册</h5>
       </div>
-
       <div class="card-body">
+        @include('shared._errors')
         <form method="POST" action="{{ route('users.store')}} ">
           {{ csrf_field() }}
           <div class="form-group">
