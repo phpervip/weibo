465303046337d22d5c064881ecac30f692032b49
phpervip <2998658517@qq.com>
2019-07-23 05:47:47 +0000

    用户显示页面

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,6 @@ Route::get('/about','StaticPagesController@about')->name('about');
 
 Route::get('signup','UsersController@create')->name('signup');
 
+Route::resource('users','UsersController');
+
 
