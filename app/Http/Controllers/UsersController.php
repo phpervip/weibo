78046f391637f4bcb10ee57c7347a90db560d02f
phpervip <2998658517@qq.com>
2019-07-24 03:17:28 +0000

    更改用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -40,6 +40,27 @@ class UsersController extends Controller
         return redirect()->route('users.show',[$user]);
     }
 
+    public function edit(User $user)
+    {
+        return view('users.edit',compact('user'));
+    }
+
+
+    public function update(User $user, Request $request)
+    {
+        $this->validate($request, [
+            'name'=>'required|max:50',
+            'password'=>'nullable|confirmed|min:6',
+        ]);
+
+        $user->update([
+            'name'=>$request->name,
+            'password'=>bcrypt($request->password),
+        ]);
+
+        return redirect()->route('users.show',$user->id);
+
+    }
 
 
 }
