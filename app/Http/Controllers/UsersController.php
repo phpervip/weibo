e4979ccf353dcfd27eadc54cf0d7f6ba116b5e62
phpervip <2998658517@qq.com>
2019-07-25 05:36:17 +0000

    查看用户列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,25 @@ use Auth;
 class UsersController extends Controller
 {
     //
+
+    public function __construct()
+    {
+        $this->middleware('auth',[
+           'except'=>['show','create','store','index']
+        ]);
+
+        $this->middleware('guest',[
+            'only'=>['create']
+        ]);
+
+    }
+
+    public function index()
+    {
+        $users = User::paginate(10);
+        return view('users.index',compact('users'));
+    }
+
     public function create()
     {
         return view('users.create');
@@ -42,12 +61,14 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+        $this->authorize('update',$user);
         return view('users.edit',compact('user'));
     }
 
 
     public function update(User $user, Request $request)
     {
+        $this->authorize('update',$user);
         $this->validate($request, [
             'name'=>'required|max:50',
             'password'=>'nullable|confirmed|min:6',
@@ -57,9 +78,7 @@ class UsersController extends Controller
             'name'=>$request->name,
             'password'=>bcrypt($request->password),
         ]);
-
         return redirect()->route('users.show',$user->id);
-
     }
 
 
